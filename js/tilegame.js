d1733eb6290029c647fe9f6a8c8d3d67a2bdf8ae
Matthew Fischer <mattfisc@gmail.com>
2020-07-28 15:41:42 -0700

    website works, need to fix the login

--- js/tilegame.js
+++ js/tilegame.js
@@ -25,23 +25,23 @@ image[0].onload = function(){
                                 image[8].onload = function(){
                                     draw_game();
                                 };
-                                image[8].src = "image/ass6p8.png";
+                                image[8].src = "img/ass6p8.png";
                             };
-                            image[7].src = "image/ass6p7.png";
+                            image[7].src = "img/ass6p7.png";
                         };
-                        image[6].src = "image/ass6p6.png";
+                        image[6].src = "img/ass6p6.png";
                     };
-                    image[5].src = "image/ass6p5.png";
+                    image[5].src = "img/ass6p5.png";
                 };
-                image[4].src = "image/ass6p4.png";
+                image[4].src = "img/ass6p4.png";
             };
-            image[3].src = "image/ass6p3.png";
+            image[3].src = "img/ass6p3.png";
         };
-        image[2].src = "image/ass6p2.png";
+        image[2].src = "img/ass6p2.png";
     };
-    image[1].src = "image/ass6p1.png";
+    image[1].src = "img/ass6p1.png";
 };
-image[0].src = "image/ass6p0.png";
+image[0].src = "img/ass6p0.png";
 
 function draw_image(index, position){
     context.drawImage(image[index],(position%3)*144,Math.floor(position/3)*144);
